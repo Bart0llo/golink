754aee13a826dcdca2eedb0ec9b24caee6615019
bart0llo <bartekn041@gmail.com>
2023-12-07 22:09:47 +0100

    config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
-const { withPlausibleProxy } = require("next-plausible");
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-module.exports = withPlausibleProxy()({});
+module.exports = nextConfig
