3c08374523c6cb692beb02aa70682685cba8ba31
bart0llo <bartekn041@gmail.com>
2023-12-07 22:26:13 +0100

    script

--- next.config.js
+++ next.config.js
@@ -2,6 +2,5 @@ const { withPlausibleProxy } = require("next-plausible");
 
 module.exports = withPlausibleProxy({
   customDomain: "https://a.bart0llo.dev",
-  scriptName: 'plausible.js',
-  subdirectory: 'js'
+  scriptName: "kielbasa.js",
 })({});
