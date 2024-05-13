accd0fd712d228983c5aa54f28546ebc0008eff2
bart0llo <bartekn041@gmail.com>
2024-05-13 21:56:48 +0200

    fix(backend): add global prefix

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -8,6 +8,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  app.setGlobalPrefix('api');
+
   app.useGlobalPipes(
     new ValidationPipe({ transform: true, stopAtFirstError: true }),
   );
