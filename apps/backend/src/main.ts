47035b3b4e744460a295234962234dde26d7db9a
bart0llo <bartekn041@gmail.com>
2024-05-13 14:38:31 +0200

    [backend]: add init packages

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import * as compression from 'compression';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({ transform: true, stopAtFirstError: true }),
+  );
+
+  /* Middleware: Compression and Helmet */
+  app.use(compression());
+  app.use(helmet());
+
+  /* Cross-Origin Resource Sharing (CORS) */
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    credentials: true,
+  });
 
   app.set('trust proxy', 1);
 
