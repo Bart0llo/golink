f23b93ea45385e72b3546187771a3525cd596d57
bart0llo <bartekn041@gmail.com>
2024-05-13 20:42:55 +0200

    [frontend]: redirect

--- apps/frontend/src/app/(main)/link/[id]/page.tsx
+++ apps/frontend/src/app/(main)/link/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { API_URL, REDIRECT_SHORT_URL } from "@/utils/constants";
 import { redirect } from "next/navigation";
 
 export default async function ShortRedirect({
@@ -6,12 +7,22 @@ export default async function ShortRedirect({
   params: { id: string };
 }) {
   const { id } = params;
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/id/${id}`);
+  const res = await fetch(`${API_URL}/${REDIRECT_SHORT_URL}/${id}`);
 
   const data = await res.json();
 
-  if (data.statusCode == 200) {
-    return redirect(data.data.target);
+  if (res.status == 200) {
+    if (
+      !data.url.startsWith("http://") &&
+      !data.url.startsWith("https://")
+    ) {
+      return redirect("https://" + data.url);
+    } else if (
+      data.url.startsWith("http://") ||
+      data.url.startsWith("https://")
+    ) {
+      return redirect(data.url);
+    }
   }
 
   return redirect("/?error=404");
--- apps/frontend/src/utils/constants.ts
+++ apps/frontend/src/utils/constants.ts
@@ -1,3 +1,4 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export const REDIRECT_URL = process.env.NEXT_PUBLIC_REDIRECT_URL;
 export const SHORT_URL = "url/short";
+export const REDIRECT_SHORT_URL = "url/redirect";
