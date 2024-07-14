eaff93748558d7fdc4e0284f9071e5ca315199dc
bart0llo <bartekn041@gmail.com>
2024-07-14 13:49:10 +0200

    frontend: golink logo

--- frontend/src/container/main-page/index.tsx
+++ frontend/src/container/main-page/index.tsx
@@ -1,12 +1,13 @@
 import style from "./style.module.css";
 import { Box, Container, Title, Image, Text } from "@mantine/core";
 import ShortForm from "./components/ShortForm";
+import GolinkBig from "@/components/logo/GolinkBig";
 
 export default function MainPageSection() {
   return (
     <Container className={style.mainBox}>
       <Box className={style.content}>
-        <Image p="sm" src="/logo.svg" alt="Logo" maw={270} fit="contain" />
+        <GolinkBig size={270} />
         <Title order={3} ta="center" px="xl">
           Return of the iconic link shortener{" "}
           <Text span inherit c="pink.6" fw="bolder">
