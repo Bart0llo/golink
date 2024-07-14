96fa6cb8183fffc94663675c3f65531505077584
bart0llo <bartekn041@gmail.com>
2024-07-14 15:28:36 +0200

    frontend: header for desktop

--- frontend/src/app/(main)/layout.tsx
+++ frontend/src/app/(main)/layout.tsx
@@ -2,14 +2,29 @@
 import { AppShell } from "@mantine/core";
 
 import styles from "./layout.module.css";
+import { usePathname } from "next/navigation";
+import Header from "@/components/Header/Header";
 
 export default function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+  const pathIsRedirect = pathname?.startsWith("/link");
+
   return (
-    <AppShell className={styles.backgroundImg}>
+    <AppShell
+      disabled={pathIsRedirect}
+      header={{ height: 80 }}
+      className={styles.backgroundImg}
+    >
+      <AppShell.Header
+        withBorder={false}
+        style={{ backgroundColor: "transparent" }}
+      >
+        <Header />
+      </AppShell.Header>
       <AppShell.Main>{children}</AppShell.Main>
     </AppShell>
   );
---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,39 @@
+import { Burger, Container, Flex, Group, UnstyledButton } from "@mantine/core";
+import Link from "next/link";
+import GolinkBig from "../logo/GolinkBig";
+
+import style from "./header.module.css";
+import { usePathname } from "next/navigation";
+
+const links = [
+  { link: "/", name: "Home" },
+  { link: "/statistics", name: "URL Stats" },
+  { link: "/reports", name: "Report URL" },
+];
+
+export default function Header() {
+  const pathname = usePathname();
+
+  const items = links.map((link) => (
+    <UnstyledButton
+      component={Link}
+      href={link.link}
+      key={link.link}
+      className={style.link}
+      data-active={link.link === pathname ? true : false}
+    >
+      {link.name}
+    </UnstyledButton>
+  ));
+
+  // Create navbar for mobile
+
+  return (
+    <Container size="md" className={style.container}>
+      <GolinkBig size={130} />
+      <Group visibleFrom="xs" gap={5}>{items}</Group>
+      
+      <Burger size="md" hiddenFrom="xs" />
+    </Container>
+  );
+}
