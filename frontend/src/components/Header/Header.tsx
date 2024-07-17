c30bfa3a88a87f4f8823a530f4b03c52d9d37ba1
bart0llo <bartekn041@gmail.com>
2024-07-17 23:11:05 +0200

    frontend: finish menu mobile

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,6 +1,14 @@
-import { Burger, Container, Flex, Group, UnstyledButton } from "@mantine/core";
+import {
+  Burger,
+  Container,
+  Drawer,
+  Group,
+  Stack,
+  UnstyledButton,
+} from "@mantine/core";
 import Link from "next/link";
 import GolinkBig from "../logo/GolinkBig";
+import { useDisclosure } from "@mantine/hooks";
 
 import style from "./header.module.css";
 import { usePathname } from "next/navigation";
@@ -12,6 +20,7 @@ const links = [
 ];
 
 export default function Header() {
+  const [opened, { toggle, close }] = useDisclosure(false);
   const pathname = usePathname();
 
   const items = links.map((link) => (
@@ -26,14 +35,22 @@ export default function Header() {
     </UnstyledButton>
   ));
 
-  // TODO: Create navbar for mobile
-
   return (
-    <Container size="md" className={style.container}>
-      <GolinkBig size={130} />
-      <Group visibleFrom="xs" gap={5}>{items}</Group>
-      
-      <Burger size="md" hiddenFrom="xs" />
-    </Container>
+    <>
+      <Container size="md" className={style.container}>
+        <GolinkBig size={130} />
+        <Group visibleFrom="xs" gap={5}>
+          {items}
+        </Group>
+
+        <Burger size="md" hiddenFrom="xs" onClick={toggle} opened={opened} />
+      </Container>
+
+      <Drawer opened={opened} onClose={close}>
+        <Stack mt="lg" gap={5}>
+          {items}
+        </Stack>
+      </Drawer>
+    </>
   );
 }
