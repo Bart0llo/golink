78113c31f8d45865e87df9d7c658c0b2df0039ee
bart0llo <bartekn041@gmail.com>
2024-07-17 21:57:42 +0200

    frontend: add links version

--- frontend/src/lib/utils/userLinks.ts
+++ frontend/src/lib/utils/userLinks.ts
@@ -1,8 +1,13 @@
 import { ShortUrl } from "@/lib/types/short";
 
+const LINKS_VERSION = "v1";
+const LINKS_KEY = `links-${LINKS_VERSION}`;
+
 export function userLinks() {
   try {
-    const links: ShortUrl[] = JSON.parse(localStorage.getItem("links") || "[]");
+    const links: ShortUrl[] = JSON.parse(
+      localStorage.getItem(LINKS_KEY) || "[]"
+    );
 
     return links;
   } catch (error) {
@@ -12,7 +17,9 @@ export function userLinks() {
 
 export function addLink(link: ShortUrl) {
   try {
-    const links: ShortUrl[] = JSON.parse(localStorage.getItem("links") || "[]");
+    const links: ShortUrl[] = JSON.parse(
+      localStorage.getItem(LINKS_KEY) || "[]"
+    );
     localStorage.setItem("links", JSON.stringify([...links, link]));
   } catch (error) {
     console.error(error);
